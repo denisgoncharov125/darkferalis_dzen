8692ab7c18c8b0933df1a3ed9710b3fe58266bbd
Denis Goncharov <Goncharov.DV2@dns-shop.ru>
2023-02-08 21:28:44 +1000

    Новые функции для тестирования Python 3.11

--- Python/001_Python_3_11/01_Execution_speed/01_math_speed_up.py
+++ Python/001_Python_3_11/01_Execution_speed/01_math_speed_up.py
@@ -1,13 +1,58 @@
+import math
 import time
 
-if __name__ == '__main__':
+RANGE_CNT = 400000000
 
-    # s = 0 Обычный цикл и обычная математика
+
+def test_funct(i: int) -> int:
+    return math.sqrt(test_funct2(i))
+
+
+def test_funct2(k: int) -> int:
+    return math.pow(k, 0.335)
+
+
+def time_process_decorator(func):
+    """Декоратор для рамера времени выполнения"""
+
+    def _wrapper(*args, **kwargs):
+        print("\n-----------------------------------------------------------------------------------------")
+        print(f"{func.__doc__} [{func.__name__}]")  # Отобразим информацию о функции
+        start_time = time.perf_counter()  # Засечем время выполнения
+        func(*args, **kwargs)  # Сделаем вызов самой функции из декоратора
+        print(f"\nВремя выполнения {time.perf_counter() - start_time:.3f} сек.")
+        print("-----------------------------------------------------------------------------------------")
+
+    return _wrapper
+
+
+@time_process_decorator
+def test_cycle_math():
+    """Тестирование простого цикла с математикой"""
     s = 0
-    start_time = time.perf_counter()
-
-    for i in range(100000000):
+    for i in range(RANGE_CNT):
         s += i
     print(f"Результат = {s}")
 
-    print(f"Время выполнения {time.perf_counter() - start_time:.3f} сек.")
+
+@time_process_decorator
+def test_cycle_2_function():
+    """Тестирование цикла с двумя вложенными функциями"""
+    s = 0
+    for i in range(RANGE_CNT):
+        s += test_funct(i)
+    print(f"Результат = {s}")
+
+
+@time_process_decorator
+def test_assignment_by_index():
+    """Тестирование присвоение коллекции по индексу"""
+    t_list = [None] * RANGE_CNT
+    for i in range(RANGE_CNT):
+        t_list[i] = i
+
+
+if __name__ == '__main__':
+    #test_cycle_math()
+    #test_cycle_2_function()
+    test_assignment_by_index()
